ef01a3a499e79355bbd94d6272a32064fc846b44
mettoboshi <mettoboshi.1108@gmail.com>
2023-10-26 06:29:44 +0900

    toolkitをインストールするユーザーを指定できるようにした

--- isucon-tools/mitamae/cookbooks/tideways_toolkit/default.rb
+++ isucon-tools/mitamae/cookbooks/tideways_toolkit/default.rb
@@ -1,10 +1,25 @@
-# Goのインストール
+# 変数
+user_name = "isucon"
+user_home = "/home/#{user_name}"
+gopath_bin = "$(go env GOPATH)/bin"
+
+# Goの環境がインストールされていることを確認
 package 'golang' do
   action :install
 end
 
 # tideways/toolkit のインストール
 execute "Install tideways toolkit using go" do
+  user user_name
+  environment 'HOME' => user_home
   command "go install github.com/tideways/toolkit@latest"
-  not_if "test -e $(go env GOPATH)/bin/twcli"
+  not_if "test -e #{gopath_bin}/toolkit"
+end
+
+# ユーザーのPATHにtideways/toolkitのパスを追加
+execute "Add GOPATH/bin to #{user_name}'s PATH" do
+  user user_name
+  environment 'HOME' => user_home
+  command %Q(echo 'export PATH="$PATH:#{gopath_bin}"' >> #{user_home}/.bashrc)
+  not_if %Q(grep -q "#{gopath_bin}" #{user_home}/.bashrc)
 end
